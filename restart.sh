ccb0af5f1c07d6c5e74177eb34899d7a0978b1d3
ethan-georlette <ethan.georlette1@gmail.com>
2026-02-19 11:00:15 +0200

    f

--- restart.sh
+++ restart.sh
@@ -5,7 +5,7 @@ echo -e "\033[0;31#####################starting docker containers...\033[0m"]
 docker kill minio  
 docker kill traefik
 echo -e "\033[0;32m"
-docker docker rm minio
+docker rm minio
 docker rm traefik
 cd docker/traefik
 docker compose up -d
