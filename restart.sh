1375b05e26ddd550d88633bcd3d81954f6224d0e
ethan-georlette <ethan.georlette1@gmail.com>
2026-02-19 10:58:18 +0200

    still

--- restart.sh
+++ restart.sh
@@ -8,7 +8,7 @@ echo -e "\033[0;32m"
 docker docker rm minio
 docker rm traefik
 cd docker/traefik
-docker-compose up -d
+docker compose up -d
 cd ../minio
-docker-compose up -d
+docker compose up -d
 echo -e "\033[0;32m###############################################done! \033[0m"]
