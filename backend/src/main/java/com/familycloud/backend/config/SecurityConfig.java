f5788b3f50b18195fc48a899606b7a61c99f6073
ethan-georlette <ethan.georlette1@gmail.com>
2026-03-25 11:22:23 +0200

    d

--- backend/src/main/java/com/familycloud/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/familycloud/backend/config/SecurityConfig.java
@@ -1,11 +1,10 @@
 package com.familycloud.backend.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
 @Configuration
 public class SecurityConfig {
@@ -14,12 +13,12 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
     @Bean
     public FilterRegistrationBean<JwtFilter> jwtFilter(JwtFilter jwtFilter) {
-    	FilterRegistrationBean<JwtFilter> registration = new FilterRegistrationBean<>();
-  	registration.setFilter(jwtFilter);
-	registration.addUrlPatterns("/*");
-    	return registration;
+        FilterRegistrationBean<JwtFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(jwtFilter);
+        registration.addUrlPatterns("/*");
+        return registration;
     }
 }
-
