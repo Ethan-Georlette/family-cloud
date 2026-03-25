c208cc759672970cf3aef5c96cde88d22d5a3e3b
ethan-georlette <ethan.georlette1@gmail.com>
2026-03-25 11:18:31 +0200

    api token added

---
+++ backend/src/main/java/com/familycloud/backend/config/JwtFilter.java
@@ -0,0 +1,54 @@
+package com.familycloud.backend.config;
+
+import com.familycloud.backend.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    public JwtFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String path = request.getRequestURI();
+
+        // Allow login & register without token
+        if (path.contains("/login") || path.equals("/api/users")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String header = request.getHeader("Authorization");
+
+        if (header == null || !header.startsWith("Bearer ")) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        String token = header.substring(7);
+
+        try {
+            jwtService.extractEmail(token);
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
--- backend/src/main/java/com/familycloud/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/familycloud/backend/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @Configuration
 public class SecurityConfig {
@@ -11,6 +13,13 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public FilterRegistrationBean<JwtFilter> jwtFilter(JwtFilter jwtFilter) {
+    	FilterRegistrationBean<JwtFilter> registration = new FilterRegistrationBean<>();
+  	registration.setFilter(jwtFilter);
+	registration.addUrlPatterns("/*");
+    	return registration;
     }
 }
 
