f7b6be81d9a2a86f314515de7469eab66e420f6f
ethan-georlette <ethan.georlette1@gmail.com>
2026-04-09 15:22:27 +0300

    wtf

--- frontend/src/config/keycloak.js
+++ frontend/src/config/keycloak.js
@@ -1,9 +1,9 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-    url: "https://auth.ethangeorlette.com",   // your Keycloak URL
-    realm: "family-cloud",          // your Keycloak realm
-    clientId: "account"
+    url: "https://auth.ethangeorlette.com",
+    realm: "family-cloud",
+    clientId: "family-cloud-frontend"
 });
 
 export default keycloak;
