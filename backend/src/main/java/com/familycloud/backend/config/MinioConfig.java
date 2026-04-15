b7f0342c4024e674843882e40cc27a5eac10f782
ethan-georlette <ethan.georlette1@gmail.com>
2026-04-15 16:13:26 +0300

    s

--- backend/src/main/java/com/familycloud/backend/config/MinioConfig.java
+++ backend/src/main/java/com/familycloud/backend/config/MinioConfig.java
@@ -1,5 +1,6 @@
 package com.familycloud.backend.config;
 
+
 import io.minio.MinioClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
